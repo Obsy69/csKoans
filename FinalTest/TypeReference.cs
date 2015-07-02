fefd0854fd3e0526ea7f06b3dda056d6f8922edb
unknown <p1104348@IUTDOUAB2PC16.univ-lyon1.fr>
2015-07-02 09:04:33 +0200

    BaseCSHarpTests DefinirUnTypeRefAvecEg.. OK

---
+++ FinalTest/TypeReference.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FinalTest
+{
+    public class TypeReference
+    {
+        private int _valeur;
+
+        public TypeReference (int valeur)
+        {
+            _valeur = valeur;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this._valeur == ((TypeReference)obj)._valeur;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ FinalTest/TypeValeur.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace FinalTest
+{
+    public struct TypeValeur
+    {
+        private int _valeur;
+
+        public TypeValeur(int valeur)
+        {
+            _valeur = valeur;
+        }
+    }
+}
