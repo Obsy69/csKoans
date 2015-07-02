a20df0970ba3570cb8ed058b1f8f4c417a0303f4
unknown <p1104348@IUTDOUAB2PC16.univ-lyon1.fr>
2015-07-02 10:23:27 +0200

    LinqTests RecupNombresPairs OK

---
+++ FinalTest/Nombres.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FinalTest
+{
+    public class Nombres
+    {
+        private readonly IEnumerable<KeyValuePair<string, int>> _keyValuePairs;
+        
+        public Nombres(List<KeyValuePair<string, int>> keyValuePairs)
+        {
+            _keyValuePairs = keyValuePairs;
+        }
+
+        public IEnumerable<int> NombresPairs
+        {
+            get { return _keyValuePairs.Select(s => s.Value).Where(s => (s % 2 == 0)); }
+        }
+    }
+}
