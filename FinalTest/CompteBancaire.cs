92cbf35228e20505616b2aed04f94c3135eb0065
unknown <p1104348@IUTDOUAB2PC16.univ-lyon1.fr>
2015-07-02 11:15:55 +0200

    PatternsTests OuvrirUnCompteBancaireProdEvt OK

---
+++ FinalTest/CompteBancaire.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace FinalTest
+{
+    public class CompteBancaire
+    {
+        private static string _numéroDeCompte;
+        private static int _autorisationDeCrédit;
+
+        public static IEnumerable<IEvenementMetier> Ouvrir(string numéroDeCompte, int autorisationDeCrédit)
+        {
+            _numéroDeCompte = numéroDeCompte;
+            _autorisationDeCrédit = autorisationDeCrédit;
+
+            var nouveauCompte = new CompteCréé(_numéroDeCompte,_autorisationDeCrédit);
+
+            yield return nouveauCompte;
+        }
+    }
+
+}
---
+++ FinalTest/CompteCréé.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalTest
+{
+    public struct CompteCréé : IEvenementMetier
+    {
+        private string _numéroDeCompte;
+        private int _autorisationDeCrédit;
+
+        public CompteCréé(string numéroDeCompte, int autorisationDeCrédit)
+        {
+            this._numéroDeCompte = numéroDeCompte;
+            this._autorisationDeCrédit = autorisationDeCrédit;
+        }
+
+    }
+}
